e82c0b636e7af381be5b66acbf10a75b0cfeb03c
herndont <herndontrey@gmail.com>
2019-04-30 07:09:28 -0700

    lab complete

---
+++ src/main/java/com/example/songr/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.songr;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping("/capitalize/{param}")
+    @ResponseBody
+    public String toStringCaps(@PathVariable String param) {
+        return param.toUpperCase();
+    }
+}
