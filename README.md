32340469f4a88551cb316765ac6fb6a2c9fe07c8
herndont <herndontrey@gmail.com>
2019-05-02 13:03:31 -0700

    have added code today but still dont have anything working- less working today than I had working yesterday

--- README.md
+++ README.md
@@ -10,3 +10,17 @@
 
 ### How to run the application
     - That is a really good question. Will get back to you on that later.
+    
+## Lab 13: Related Resources and Integration Tests
+    This lab is currently not going well for me at all. I am completely lost and am going to spend the weekend redoing
+    labs and trying to understand this. I have watched the front row video over and over again and this is just not making 
+    sense. 
+    I will resubmit these assignments when I get something or anything working because I cannot even get anything to add
+     to the database currently.
+    
+### How to run the application
+    - Currently you cannot run the application. You should be able to hit build and run the code. This will start the
+     server and add infformation to the database which will show through the album template.
+     
+     - Lab requires that the application should be run using a single gradle command and I have not even gotten far 
+     enough to look at or understand this portion yet.
--- src/main/java/com/example/songr/controllers/AlbumController.java
+++ src/main/java/com/example/songr/controllers/AlbumController.java
@@ -1,12 +1,16 @@
 package com.example.songr.controllers;
 
+import com.example.songr.AlbumNotFoundException;
 import com.example.songr.database.Album;
 import com.example.songr.database.AlbumRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 import java.util.Optional;
 
 
@@ -18,7 +22,11 @@ public class AlbumController {
     AlbumRepository repo;
 
     @GetMapping("/")
-    public Iterable<Album> getAlbums(){
+    public String getAlbums(Model model){
+        List<Album> albums = (List<Album>) this.repo.findAll();
+        model.addAttribute("albums", albums);
+
+
         Album album = new Album();
         album.title = "Led Zeppelin III";
         album.artist = "Led Zeppelin";
@@ -27,9 +35,51 @@ public class AlbumController {
         album.imageURL = "https://en.wikipedia.org/wiki/Led_Zeppelin_III#/media/File:Led_Zeppelin_-_Led_Zeppelin_III.png";
 
         this.repo.save(album);
+        //Returns all the albums including the new one
 
-        Iterable<Album> albums = this.repo.findAll();
-
-        return albums;
+        return "albums";
     }
+
+//    @GetMapping("/album/{id}")
+//    public Album getAlbum(@PathVariable Long id) {
+//    //Getting the Album by id
+//        Optional<Album> album = this.repo.findById(id);
+//        if (album.isPresent()) {
+//            //return value of Album Optional
+//            return album.get();
+//        } else {
+//            throw new AlbumNotFoundException();
+//        }
+//    }
+//
+//    @PostMapping("/album")
+//    public Album createAlbum(@RequestBody Album album){
+//        album = this.repo.save(album);
+//        return album;
+//    }
+
+//    @PutMapping("/album/{id}")
+//    public Album updateAlbum(@PathVariable Long id, @RequestBody Album album) {
+//        //Make sure the album exists and make sure they are not passing something we don't want in.
+//        //We don't want to save something bad
+//        Optional<Album> repoAlbum = this.repo.findById(id);
+//        if (repoAlbum.isPresent()) {
+//            Album foundAlbum = repoAlbum.get();
+//
+//            foundAlbum.title = album.title;
+//            foundAlbum.artist = album.artist;
+//            foundAlbum.songCount = album.songCount;
+//            foundAlbum.length = album.length;
+//            foundAlbum.imageURL = album.imageURL;
+//
+//            foundAlbum = this.repo.save(foundAlbum);
+//            return foundAlbum;
+//        }
+//        throw new AlbumNotFoundException();
+//    }
+//
+//    @DeleteMapping("/album/{id}")
+//    public void deleteAlbum(@PathVariable Long id) {
+//        this.repo.deleteById(id);
+//    }
 }
