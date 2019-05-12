84c9f464b75a1d20b8a495e0b184b759b4f3a51c
herndont <herndontrey@gmail.com>
2019-05-12 13:24:41 -0700

    finised out the songr labs... Going to turn this branch in for redos on all

--- src/main/java/com/example/songr/controllers/AlbumController.java
+++ src/main/java/com/example/songr/controllers/AlbumController.java
@@ -3,13 +3,14 @@ package com.example.songr.controllers;
 import com.example.songr.AlbumNotFoundException;
 import com.example.songr.database.Album;
 import com.example.songr.database.AlbumRepository;
+import com.example.songr.database.Song;
+import com.example.songr.database.SongRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
@@ -21,6 +22,9 @@ import java.util.Optional;
 public class AlbumController {
     @Autowired
     AlbumRepository repoAlbum;
+
+    @Autowired
+    SongRepository repoSong;
 
     @GetMapping("")
     public String getAlbums(Model model){
@@ -46,7 +50,11 @@ public class AlbumController {
         Optional<Album> album = this.repoAlbum.findById(id);
         if (album.isPresent()) {
             //return value of Album Optional
-            model.addAttribute("album", album);
+            model.addAttribute("album", album.get());
+
+            List<Song> songs = (List<Song>) this.repoSong.findAll();
+
+            model.addAttribute(songs);
             return "singleAlbum";
         } else {
             throw new AlbumNotFoundException();
@@ -70,30 +78,5 @@ public class AlbumController {
 
         repoAlbum.save(album);
         return new RedirectView("") ;
-    }
-
-    @PutMapping("/{id}")
-    public Album updateAlbum(@PathVariable Long id, @RequestBody Album album) {
-        //Make sure the album exists and make sure they are not passing something we don't want in.
-        //We don't want to save something bad
-        Optional<Album> repoAlbum = this.repoAlbum.findById(id);
-        if (repoAlbum.isPresent()) {
-            Album foundAlbum = repoAlbum.get();
-
-            foundAlbum.title = album.title;
-            foundAlbum.artist = album.artist;
-            foundAlbum.songCount = album.songCount;
-            foundAlbum.length = album.length;
-            foundAlbum.imageURL = album.imageURL;
-
-            foundAlbum = this.repoAlbum.save(foundAlbum);
-            return foundAlbum;
-        }
-        throw new AlbumNotFoundException();
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteAlbum(@PathVariable Long id) {
-        this.repoAlbum.deleteById(id);
     }
 }
